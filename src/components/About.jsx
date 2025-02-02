5785547c91f305ef5638382027ce53b27d2b260a
Ashish <ashishrouniyar2@gmail.com>
2025-02-02 10:32:08 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Ashish, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+         
+          [Master’s In Computer Application  2023 - 2025, Lovely Professional University]<br></br>
+          [Bachelore in Computer Application 2020 - 2023, University Of North Bengal]
+          
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in [C++, Html, CSS,  JavaScript, React js] <br></br>
+           Software
+          Tools/Technologies: [Angular, React JS, Node JS, Selenium, Git, GitHub, Figma, Adobe xd, VS code.] 
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+        Full Stack Developer Intern	May 2023 - July 2023
+Placify Technologies Pvt. Ltd.<br></br>
+•	Developed a comprehensive Task Management application using Angular, Node.js, Express.js<br></br>
+•	Implemented a robust API to manage tasks with features including adding, updating, sorting, and exporting tasks.<br></br>
+•	While ensuring a user-friendly interface and seamless integration between the frontend and Backend. 
+
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Certificates
+        </h1>
+        <span>
+          React Basics by (Coursera)     August 2024<br></br>
+          C++ by (Saylors Academy)     March 2024 <br></br>
+          UX/UI Design- Mastery by (Udemy)     January 2023       
+         
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative IT solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
+
